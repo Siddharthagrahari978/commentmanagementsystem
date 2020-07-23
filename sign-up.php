431e7ef323a29811845e3b5fd0a4a7cf6ee7d2d5
Siddharth Kumar <43567482+Siddharthagrahari978@users.noreply.github.com>
2020-07-24 03:49:50 +0530

    Update sign-up.php

--- sign-up.php
+++ sign-up.php
@@ -1,8 +1,8 @@
 <?php  
  $message = '';  
  $error = ''; 
-$array_data = "";
-
+ $array_data = "";
+ $ref=0;
  if(isset($_POST["submit"]))  
  {  
       if(empty($_POST["username"]))  
@@ -26,23 +26,32 @@ $array_data = "";
            if(file_exists('database.json'))  
            {  
                 $current_data = file_get_contents('database.json');  
-                $array_data = json_decode($current_data, true);  
-                $extra = array(  
-                     'username'               =>     $_POST['username'],
-				 'first_name'			 =>	   $_POST['first_name'],
-				 'last_name'			 =>	   $_POST['last_name'],
-                     'password'           =>     $_POST["password"],
-				 'phone'			  =>	    $_POST["phone"],
-				 'address'		  =>	    $_POST["address"],
-				 'comment'		  =>     array()
-                );  
-                $array_data[] = $extra;  
-                $final_data = json_encode($array_data);  
-                if(file_put_contents('database.json', $final_data))  
-                {  
-                     $message = "<div class="alert alert-success" role="alert">Signup Successfull...</div>";  
-                }  
-           }  
+                $array_data = json_decode($current_data, true);
+                for( $i = 0 ; $i < count($array_data) ; $i++){
+	               if($array_data[$i]['username'] == $_POST["username"]){
+                         $error='<div class="alert alert-warning" role="alert">User Already Exists.</div>';
+                         $ref=1;
+                         break;
+                    }
+               }
+               if($ref==0){
+                    $extra = array(  
+                         'username'           =>     $_POST['username'],
+                         'first_name'		  =>	    $_POST['first_name'],
+                         'last_name'		  =>	    $_POST['last_name'],
+                         'password'           =>     $_POST["password"],
+                         'phone'			  =>	    $_POST["phone"],
+                         'address'		  =>	    $_POST["address"],
+                         'comment'		  =>     array()
+                    );  
+                    $array_data[] = $extra;  
+                    $final_data = json_encode($array_data);  
+                    if(file_put_contents('database.json', $final_data))  
+                    {  
+                         $message = "<div class='alert alert-success' role='alert'>Signed up Successfully</div>";  
+                    }  
+               }
+          }
            else  
            {  
                 $error = 'JSON File not exits';  
@@ -56,7 +65,7 @@ $array_data = "";
 <!DOCTYPE html>  
  <html>  
       <head>  
-           <title>Sign Up with us</title>  
+           <title>Webslesson Tutorial | Append Data to JSON File using PHP</title>  
            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
@@ -64,8 +73,8 @@ $array_data = "";
       <body>  
            <br />  
            <div class="container" style="width:500px;">  
-                <h3 align="">Sign Up!</h3><br /> 
-		<?php if(isset($message)){echo $message."\n";}?>
+                <h3 align="">Sign Up!!!</h3><br />   
+                <?php if(isset($message)) { echo $message."\n"; } ?>
                 <form method="post">  
                      <?php   
                      if(isset($error))  
@@ -89,10 +98,9 @@ $array_data = "";
 				 
                      <input type="submit" name="submit" value="Signup" class="btn btn-success" /><br />  
                      
-		     <br />
-		     <a class="btn btn-info" href="/">Back to login page</a>
-		     
-                     
+			   	<br />
+			   	<a class="btn btn-info" href="index.php?info=1">&lt; Back to sign in page</a>
+                      
                 </form>  
            </div>  
            <br />  
