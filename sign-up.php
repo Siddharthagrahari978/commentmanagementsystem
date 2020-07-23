419b0235713fc023cffb484a51e2d5cdb7507d27
Siddharth Kumar <43567482+Siddharthagrahari978@users.noreply.github.com>
2020-07-24 03:50:21 +0530

    Update sign-up.php

--- sign-up.php
+++ sign-up.php
@@ -65,7 +65,7 @@
 <!DOCTYPE html>  
  <html>  
       <head>  
-           <title>Webslesson Tutorial | Append Data to JSON File using PHP</title>  
+           <title>Sign up!</title>  
            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
